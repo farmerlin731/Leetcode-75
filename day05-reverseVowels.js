13d5c6a92483ff4f245a38160f75822361363a90
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-10-07 11:13:11 +0800

    Day05: 345. Reverse Vowels of a String_ver2 (10/05)

--- day05-reverseVowels.js
+++ day05-reverseVowels.js
@@ -7,9 +7,30 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function (s) {};
+var reverseVowels = function (s) {
+  const vowelsSet = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+
+  const sArr = [...s];
+  let left = 0,
+    right = s.length - 1;
+
+  while (left < right) {
+    if (!vowelsSet.has(sArr[left])) left++;
+    if (!vowelsSet.has(sArr[right])) right--;
+    if (vowelsSet.has(sArr[left]) && vowelsSet.has(sArr[right])) {
+      [sArr[left], sArr[right]] = [sArr[right], sArr[left]];
+      left++;
+      right--;
+    }
+  }
+  return sArr.join("");
+};
 
 var reverseVowelsFancySolu = function (s) {
   const vow = s.match(/[aeiou]/gi);
   return s.replace(/[aeiou]/gi, (el) => vow.pop());
 };
+
+let s = "IceCreAm";
+
+console.log(reverseVowels(s));
