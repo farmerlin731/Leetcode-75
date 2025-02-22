daea23c4e95a44e60d4c4a840ef05e1a21025c73
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-02-22 23:45:59 +0800

    Day57: 875. Koko Eating Bananas (02/22)

---
+++ day57-minEatingSpeed.js
@@ -0,0 +1,13 @@
+//875. Koko Eating Bananas
+//First Idea:
+//Binary Search? Maybe...The right side is the max of piles?
+//Solution: https://leetcode.com/problems/koko-eating-bananas/solutions/1703427/javascript-binary-search-explainedhttps://leetcode.com/problems/koko-eating-bananas/solutions/1703427/javascript-binary-search-explained
+
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function (piles, h) {};
+
+// const consumeTime =
