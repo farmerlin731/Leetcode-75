0e9d9a8a755a5de7ebd1eb96607dfa04c0d36a1a
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-02-28 17:23:09 +0800

    Day57: 875. Koko Eating Bananas_v2 (02/22)

--- day57-minEatingSpeed.js
+++ day57-minEatingSpeed.js
@@ -8,6 +8,31 @@
  * @param {number} h
  * @return {number}
  */
-var minEatingSpeed = function (piles, h) {};
+var minEatingSpeed = function (piles, h) {
+  const consumeTime = (count) =>
+    piles.reduce((sum, cur) => sum + Math.ceil(cur / count), 0);
+
+  let left = 0,
+    right = Math.max(...piles),
+    mid;
+
+  while (left < right) {
+    mid = (left + right) >> 1;
+    if (consumeTime(mid) > h) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+};
 
 // const consumeTime =
+
+// let piles = [30, 11, 23, 4, 20],
+//   h = 5;
+
+let piles = [3, 6, 7, 11],
+  h = 8;
+
+console.log(left);
