bde72fe7c06a451a2ae3ce6cca7643e6aa6920e2
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-03-07 15:56:09 +0800

    Remove abundant files.

--- tmp.js
+++
@@ -1,4 +0,0 @@
-test();
-const tmp = function test() {
-  console.log("hello world :)");
-};
--- tmp2.js
+++
@@ -1,9 +0,0 @@
-const array2 = Array.from({ length: 5 }, () => Array(5).fill(5));
-
-array2[1][1] = 888;
-console.log(array2);
-
-let n = 6;
-const cMap = Array.from({ length: n }, () => []);
-cMap[2].push(5);
-console.log(cMap);
