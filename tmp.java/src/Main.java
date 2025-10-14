01a15f375bae90e500e9967bccfa81964e1e909a
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-10-14 14:44:57 +0800

    Q206 by Java. (251014)

--- tmp.java/src/Main.java
+++ tmp.java/src/Main.java
@@ -1,22 +1,53 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+class ListNode {
+    int val;
+    ListNode next;
 
-class RecentCounter {
-    private Deque<Integer> queue;
-
-    public RecentCounter() {
-        this.queue = new ArrayDeque<>();
-    }
-
-    public int ping(int t) {
-        queue.addLast(t);
-        while (queue.peekFirst() < t - 3000) queue.removeFirst();
-        return queue.size();
+    ListNode(int val) {
+        this.val = val;
     }
 }
 
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter obj = new RecentCounter();
- * int param_1 = obj.ping(t);
- */
+public class Main {
+    public static ListNode reverseList(ListNode head) {
+        //By Recursion
+        if (head == null || head.next == null) return head;
+
+        ListNode cur = head;
+        ListNode newHead = reverseList(cur.next);
+        //The first node to be executed is the second to last node
+        cur.next.next = cur;
+        //Need to execute this line, otherwise there is error at original head.
+        cur.next = null;
+        return newHead;
+    }
+
+    // ===== 測試用 main =====
+    public static void main(String[] args) {
+        // 建立測試用鏈結串列: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("原始串列：");
+        printList(head);
+
+        // 執行反轉
+        ListNode reversed = reverseList(head);
+
+        System.out.println("反轉後串列：");
+        printList(reversed);
+    }
+
+    // 輔助方法：印出鏈結串列
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val);
+            if (curr.next != null) System.out.print(" -> ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
